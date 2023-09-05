71e11f6ccea6a000ffa9e1965ed5e474dd7c2517
ehom1 <erichom888@gmail.com>
2023-09-04 21:05:34 -0400

    first commit

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -4,3 +4,10 @@ classes = 5
 tuition = 6000
 cost_per_week = (tuition / classes) / weeks
 print("Cost per week:", cost_per_week)
+
+classes_per_week = 4
+cost_per_class = cost_per_week / classes_per_week
+print("Cost per class:", cost_per_class)
+
+print (classes_per_week, type(classes_per_week))
+print (cost_per_class, type(classes_per_week))
