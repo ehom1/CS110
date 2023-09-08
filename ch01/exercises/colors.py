ff6a7d6b776e2beaafb49c8c4d12d80003cdf60a
ehom1 <erichom888@gmail.com>
2023-09-08 12:22:46 -0400

    lab 1

---
+++ ch01/exercises/colors.py
@@ -0,0 +1,5 @@
+list = ["green", "blue", "red", "yellow"]
+list_length = len(list)
+
+print("I have", list_length, "colors")
+print(*list, sep = "\n")
--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,12 +1,23 @@
+import random
+
 # Part A
 weeks = 16
+print(weeks, type(weeks)) #weeks
 classes = 5
+print(classes, type(classes)) #classes
 tuition = 6000
+print(tuition, type(tuition)) #tuition
 cost_per_week = (tuition / classes) / weeks
+print(cost_per_week, type(cost_per_week)) #cost per week
 print("Cost per week:", cost_per_week)
 
 classes_per_week = 4
+print(classes_per_week, type(classes_per_week)) #classes per week
 cost_per_class = cost_per_week / classes_per_week
+print(cost_per_class, type(cost_per_class)) #cost per class
+print("Cost per class:", cost_per_class)
 
-print ("Number of classes per week:", classes_per_week, type(classes_per_week))
-print ("Cost per class:", cost_per_class, type(classes_per_week))
+#Part B
+list = ["chicken", "cow", "pig", "sheep", "lamb"]
+rand = random.choice(list)
+print("Random data object from list:", rand)
