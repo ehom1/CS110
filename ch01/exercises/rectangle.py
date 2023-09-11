d4cef6f83a6fd9306a31a93eaca83e258e3904e8
ehom1 <erichom888@gmail.com>
2023-09-11 17:32:04 -0400

    hexagon

--- ch01/exercises/rectangle.py
+++ ch01/exercises/rectangle.py
@@ -1,5 +1,5 @@
 shape = "hexagon"
 sides = 6
 
-intangle = ((sides - 2) * 180 / sides)
-print ("Interior angle:", intangle)
+int_angle = ((sides - 2) * 180 / sides)
+print ("Interior angle:", int_angle)
