a25c4bea0f2af409d7a49f7efc3259b303af4f38
ehom1 <erichom888@gmail.com>
2023-09-13 22:25:42 -0400

    turtle

---
+++ ch02/exercises/drawing.py
@@ -0,0 +1,17 @@
+import turtle 
+
+number_sides = int(input("Enter the number of sides: "))
+length = int(input("Enter the length of each side: "))
+
+tu = turtle.Turtle()
+screen = turtle.Screen()
+tu.shape("turtle")
+tu.color("blue")
+int_angle = 360 / number_sides
+colors = ["blue", "green", "yellow", "red"]
+
+for _ in range(number_sides):
+    tu.forward(length)
+    tu.right(int_angle)
+
+screen.exitonclick() 
