fb4bb138d9360eac5bb47129c3e8034b7f804359
ehom1 <erichom888@gmail.com>
2023-10-13 21:51:58 -0400

    events

--- ch03/exercises/simon_says.py
+++ ch03/exercises/simon_says.py
@@ -7,7 +7,6 @@ screen = pygame.display.set_mode()
 colors = ["red", "purple", "green", "blue"]
 random.shuffle(colors)
 
-
 for color in colors:
     screen.fill(color)
     pygame.display.flip()
@@ -18,38 +17,41 @@ for color in colors:
     pygame.time.wait(250)
     
 msg = """
+Your arrow keys correspond to the following colors:
     UP: red
     DOWN: purple
     LEFT: green
     RIGHT: blue
     
-    Click on the window, enter the sequence, then press <enter> in the console
+    Click on the window, enter the sequence, then press <enter> in the console.
 """
 
 input(msg)
 
 user_guess = []
-for event in pygame.event.get():
+for event in pygame.event.get(): 
     if event.type == pygame.KEYDOWN:
-        if (event.key == pygame.K_UP):
+        if(event.key == pygame.K_UP):
             screen.fill("red")
             user_guess.append("red")
-        elif (event.key == pygame.K_DOWN):
+        elif(event.key == pygame.K_DOWN):
             screen.fill("purple")
             user_guess.append("purple")
-        elif (event.key == pygame.K_LEFT):
+        elif(event.key == pygame.K_LEFT):
             screen.fill("green")
             user_guess.append("green")
-        elif (event.key == pygame.K_RIGHT):
+        elif(event.key == pygame.K_RIGHT):
             screen.fill("blue")
             user_guess.append("blue")
         pygame.display.flip()
         pygame.time.wait(1000)
         
 print("You entered:", user_guess)
-print("The correct pattern was:", colors)
+print("The correct pattern was", colors)
 
 if user_guess == colors:
     print("Correct! You win.")
 else:
-    print("Wrong, were you even paying attention?")
+    print("Were you even paying attention?")
+    
+pygame.quit()
--- ch04/exercises/event.py
+++ ch04/exercises/event.py
@@ -2,7 +2,7 @@ import pygame
 import random
 
 pygame.init()
-screen = pygame.display.set_mode((600, 400))
+screen = pygame.display.set_mode()
 width, height = pygame.display.get_window_size()  # tuple
 
 hit_box_width = width / 2
@@ -17,6 +17,8 @@ hitboxes = {
 
 # Position Hitboxes
 hitboxes["blue"].topleft = hitboxes["red"].topright
+hitboxes["green"].topright = hitboxes["red"].bottomright
+hitboxes["purple"].topleft = hitboxes["red"].bottomright
 
 # Define main colors
 main_colors = {
@@ -34,12 +36,67 @@ highlight_colors = {
     "purple": (250, 0, 250),
 }
 
-running = True
-while running:  # mainloop - 1 frame
-    # 1. event loop 
-    for event in pygame.event.get():
-        print(event)
-        if event.type == pygame.QUIT:
-            running = False
+font = pygame.font.Font(None, 48)
 
-pygame.quit()
+done = False
+result = []
+turns = 0
+order = list(hitboxes.keys())
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                random.shuffle(order)
+                turns = len(order)
+                result = []
+                for color in order:
+                    for c, hb in hitboxes.items():
+                        pygame.draw.rect(screen, main_colors[c], hb)
+                    pygame.draw.rect(screen, highlight_colors[color], hitboxes[color])
+                    pygame.display.flip()
+                    pygame.time.delay(1000)
+            elif event.key == pygame.K_q:
+                done = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            turns = turns - 1
+            if hitboxes["red"].collidepoint(event.pos):
+                result.append("red")
+            elif hitboxes["purple"].collidepoint(event.pos):
+                result.append("purple")
+            elif hitboxes["green"].collidepoint(event.pos):
+                result.append("green")
+            elif hitboxes["blue"].collidepoint(event.pos):
+                result.append("blue")
+            
+    # Update data 
+    if len(result) == len(order):
+        msg = [
+            f"You entered: {result}",
+            f"the correct pattern was: {order}",
+        ]
+        if result == order:
+            msg.append("Yay! You won.")
+        else:
+            msg.append("Um, no.")
+        msg.append("Press spacebar to play again.")
+    elif turns:
+        msg = ["Your turn"]
+    else:
+        msg = ["Press spacebar to play the game...(q to quit)"]
+    
+    # Draw screen
+    screen.fill("black")
+    for c, hb in hitboxes.items():
+        pygame.draw.rect(screen, main_colors[c], hb)
+        
+    if result:
+        pygame.draw.rect(screen, highlight_colors[result[-1]], hitboxes[result[-1]])
+        
+    ypos = 60
+    for m in msg:
+        text = font.render(m, True, "white")
+        screen.blit(text, (20, ypos))
+        ypos += 60
+        
+    pygame.display.flip()
