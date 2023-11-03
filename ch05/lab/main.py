492d65d5afc484a7483d2e7cd8c2bc4451f06066
ehom1 <erichom888@gmail.com>
2023-11-03 17:47:15 -0400

    5

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -21,27 +21,43 @@ def threenp1range(upper_limit):
 def graph_coordinates(threenplus1_iters_dict):
     pygame.init()
     
-    factor = 3
+    factor = 5
     coordinates = []
-    for key, points in threenplus1_iters_dict.items():
-        x = key
-        y = points 
-        coordinates.append((x, y))
+    
+    max_so_far = 0
     
     window = pygame.display.set_mode((900, 600))
     window.fill("white")
-    pygame.draw.lines(window, "black", False, coordinates, 2)
     
-    new_display = pygame.transform.flip(window, False, True)
-    width, height = new_display.get_size()
+    for key, value in threenplus1_iters_dict.items():
+        x = key * factor
+        y = value * factor
+        coordinates.append((x, y))
+        pygame.draw.circle(window, "blue", (x, y), 1)
+        
+        if value > max_so_far:
+            max_so_far = max(max_so_far, value)
+    
+    if len(coordinates) >= 2:
+        pygame.draw.lines(window, "black", False, coordinates, 1)
+    
+    width, height = window.get_size()
+    
+    font = pygame.font.Font(None, 40)
+    msg = font.render("Max So Far: " + str(max_so_far), True, "blue")
     
     new_display = pygame.transform.scale(window, (width * factor, height * factor))
     window.blit(new_display, (0, 0))
     
+    new_display = pygame.transform.flip(window, False, True)
+    window.blit(new_display, (0, 0))
+    window.blit(msg, (10, 10))
+    
     pygame.display.flip()
 
 def main():
-    threenplus1_iters_dict = threenp1range(20)
+    upper_limit = 25
+    threenplus1_iters_dict = threenp1range(upper_limit)
     print(threenplus1_iters_dict)
     graph_coordinates(threenplus1_iters_dict)
     
@@ -51,4 +67,5 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
     pygame.quit()
+    
 main()
